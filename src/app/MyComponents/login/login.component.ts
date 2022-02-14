ccf11a2afde1a1aa130a410d252964d4c8698504
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-14 12:50:20 +0500

    Update login.component.ts

--- src/app/MyComponents/login/login.component.ts
+++ src/app/MyComponents/login/login.component.ts
@@ -6,12 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+  userEmail: string = "ahsan@gmail.com";
+  userPass: string = "password123"
   constructor() {
-    
-   }
+
+  }
 
   ngOnInit(): void {
   }
-
+  submitForm() {
+    // console.log(this.userEmail, this.userPass);
+    // let emailVal = document.getElementById("email").value;
+    // let passVal = document.getElementById("password").value;
+    // if (emailVal === this.userEmail && passVal === this.userPass) {
+    //   console.log("Logged in")
+    // } else {
+    //   alert("Wrong username or password");
+    // }
+    // console.log(emailVal, passVal);
+    console.log("submitting form")
+  }
+  showPassword() {
+    // let passField = document.querySelector("#password");
+    // let passIcon = document.getElementsByClassName("showPass")[0];
+    // if (passField.type === "password") {
+    // 	passField.type = "text";
+    // 	passIcon.classList.remove("fa-eye");
+    // 	passIcon.classList.add("fa-eye-slash");
+    // } else {
+    // 	passField.type = "password";
+    // 	passIcon.classList.remove("fa-eye-slash");
+    // 	passIcon.classList.add("fa-eye");
+    // }
+    console.log("Showing password");
+  }
 }
